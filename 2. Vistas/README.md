74bfcb40ca9fe2bc3abd5a6ebe9cca9a0babe867
IsaelFatamaDev <isael.fatama@vallegrande.edu.pe>
2024-01-23 11:31:09 -0500

    Form - Activity

---
+++ 2. Vistas/README.md
@@ -0,0 +1,54 @@
+
+# CRUD BÁSICO EN HTML
+
+Elaboración de un crud básico en html, css y js.
+
+## Features  
+
+- Crear (CREATE)
+- Leer (READ)
+- Actualizar (UPDATE)
+- Eliminar (DELETE)
+
+## Screenshots  
+
+![App Screenshot](./public/img/Form.png)
+![App Screenshot](./public/img/Datos.png)
+![App Screenshot](./public/img/Actualizar.png)
+![App Screenshot](./public/img/Borrar.png)
+
+## Tech Stack  
+
+**Client:** Html, Css, Boostrap  
+
+**Server:** Node, Express
+
+## Elaborarlo en local
+
+Clonar el repositorio
+
+~~~bash  
+  git clone https://link-to-project
+~~~
+
+Entrar a la carpeta del repositorio
+
+~~~bash  
+  cd my-project
+~~~
+
+Instalar las dependencias  
+
+~~~bash  
+npm install
+~~~
+
+Iniciar el servidor
+
+~~~bash  
+npm run start
+~~~
+
+## License  
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- 2. Vistas/index.js
+++ 2. Vistas/index.js
@@ -1,16 +1,15 @@
-//* IMPORTACIÓN DE MÓDULOS
+// IMPORTACIÓN DE MÓDULOS
 const express = require('express');
 const app = express();
-const db = require('./config/db')
+const db = require('./config/db');
 
 app.set('view engine', 'ejs');
-app.set('views', './views')
+app.set('views', './views');
 
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
 
-//Renderizado de un archivo EJS (MOTOR DE PLANTILLAS)
+// Renderizado de un archivo EJS (MOTOR DE PLANTILLAS)
 app.get("/", (req, res) => {
      res.render('index');
 });
@@ -28,32 +27,84 @@ app.post('/guardar', (req, res) => {
                res.send('Error al guardar en la base de datos');
           } else {
                console.log('Datos guardados correctamente');
-               //res.send('Datos guardados correctamente');
-               res.render('/');
+               res.redirect('/mostrar');
           }
      });
 });
-
 
 // TRAER DATOS DE LA BASE DE DATOS
 app.get('/mostrar', (req, res) => {
      const query = 'SELECT id, nombres, apellidos, dni, DATE_FORMAT(fechaNacimiento, "%d/%m/%Y") as fechaNacimiento, DATE_FORMAT(fechaRegistro, "%d/%m/%Y %H:%i:%s") as fechaRegistro FROM datos';
      db.query(query, (err, result) => {
           if (err) {
-               console.error('Error al obtener los datos: ', err)
-               res.send('Error al obtener los datos: ', err)
+               console.error('Error al obtener los datos: ', err);
+               res.send('Error al obtener los datos: ', err);
           } else {
                res.render('mostrar', { datos: result });
           }
      });
 });
 
+// Ruta para mostrar la página de actualización
+app.get('/actualizar/:id', (req, res) => {
+     const id = req.params.id;
+     const query = 'SELECT id, nombres, apellidos, dni, DATE_FORMAT(fechaNacimiento, "%Y-%m-%d") as fechaNacimiento FROM datos WHERE id = ?';
+     db.query(query, [id], (err, result) => {
+          if (err) {
+               console.error('Error al obtener los datos: ', err);
+               res.send('Error al obtener los datos: ', err);
+          } else {
+               res.render('actualizar', { datos: result[0] });
+          }
+     });
+});
 
+// Ruta para actualizar un registro
+app.post('/actualizar/:id', (req, res) => {
+     const id = req.params.id;
+     const { nombres, apellidos, dni, fechaNacimiento } = req.body;
+     const query = 'UPDATE datos SET nombres=?, apellidos=?, dni=?, fechaNacimiento=? WHERE id=?';
 
+     db.query(query, [nombres, apellidos, dni, fechaNacimiento, id], (err, result) => {
+          if (err) {
+               console.error('Error al actualizar en la base de datos: ', err);
+               res.send('Error al actualizar en la base de datos');
+          } else {
+               console.log('Datos actualizados correctamente');
+               res.redirect('/mostrar');
+          }
+     });
+});
 
+app.get('/borrar/:id', (req, res) => {
+     const id = req.params.id;
+     const query = 'SELECT id, nombres, apellidos, dni, DATE_FORMAT(fechaNacimiento, "%d/%m/%Y") as fechaNacimiento FROM datos WHERE id = ?';
+     db.query(query, [id], (err, result) => {
+          if (err) {
+               console.error('Error al obtener los datos: ', err);
+               res.send('Error al obtener los datos: ', err);
+          } else {
+               res.render('borrar', { datos: result[0] });
+          }
+     });
+});
 
-//Iniciar Servidor
+app.post('/borrar/:id', (req, res) => {
+     const id = req.params.id;
+     const query = 'DELETE FROM datos WHERE id=?';
+     db.query(query, [id], (err, result) => {
+          if (err) {
+               console.error('Error al borrar en la base de datos: ', err);
+               res.send('Error al borrar en la base de datos');
+          } else {
+               console.log('Datos borrados correctamente');
+               res.redirect('/mostrar');
+          }
+     });
+});
+
+// Iniciar Servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-     console.log(`El servidor está funcionando en http://localhost:${PORT}`)
+     console.log(`El servidor está funcionando en http://localhost:${PORT}`);
 });
